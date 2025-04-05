965b712c07565d22c44f080fcfcb1de561512b2a
Sasha <gerasimenko-05@bk.ru>
2025-04-05 10:33:42 +0300

    10

--- client/src/components/SortPanel.jsx
+++ client/src/components/SortPanel.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+
 const SortPanel = ({ onSortChange }) => {
  const sortOptions = [
- { value: 'default', label: 'По умолчанию' },
- { value: 'priceAsc', label: 'По цене (возрастание)' },
- { value: 'priceDesc', label: 'По цене (убывание)' },
- { value: 'rating', label: 'По рейтингу' },
+   { value: 'default', label: 'По умолчанию' },
+   { value: 'priceAsc', label: 'По цене (возрастание)' },
+   { value: 'priceDesc', label: 'По цене (убывание)' },
+   // Removed sorting option for rating
  ];
- return (
- <FormControl fullWidth sx={{ my: 2 }}>
- <InputLabel>Сортировка</InputLabel>
- <Select
- label="Сортировка"
- onChange={(e) => {
-   console.log('Selected Sort Value:', e.target.value);
-   onSortChange(e.target.value);
- }}
 
- defaultValue="default"
- >
- {sortOptions.map((option) => (
- <MenuItem key={option.value} value={option.value}>
- {option.label}
- </MenuItem>
- ))}
- </Select>
- </FormControl>
+ return (
+   <FormControl fullWidth sx={{ my: 2 }}>
+     <InputLabel>Сортировка</InputLabel>
+     <Select
+       label="Сортировка"
+       onChange={(e) => {
+         console.log('Selected Sort Value:', e.target.value);
+         onSortChange(e.target.value);
+       }}
+       defaultValue="default"
+     >
+       {sortOptions.map((option) => (
+         <MenuItem key={option.value} value={option.value}>
+           {option.label}
+         </MenuItem>
+       ))}
+     </Select>
+   </FormControl>
  );
 };
+
 export default SortPanel;
